17b7f8482a806eec3c146c30671d85dbde9e66fd
kalyani joshi <kalyani121@gmail.com>
2021-10-08 15:05:31 -0400

    created Models

---
+++ server/models/Jobs.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const { Schema } = mongoose;
+
+const jobSchema = new Schema({
+    postDate: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    jobTitle: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    jobLocation: {
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    jobType: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    salary: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    jobDescription: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    skills: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    employer: {
+        type: Schema.Types.ObjectId,
+        ref: 'Employer'
+    },
+    users: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+});
+
+const Job = mongoose.model('Job', jobSchema)
+module.exports = Job;
