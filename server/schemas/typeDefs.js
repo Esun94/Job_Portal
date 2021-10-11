b0cc07f6d1e19fb6f67fedbac2f3c28dd2471703
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-10-11 12:09:23 -0400

    updated resolvers and added inputs to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,77 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type Employer {
+  _id: ID
+  companyName: String
+  address: String
+  city: String
+  state: String
+  website: String
+  userName: String
+  email: Strin
+  password: String
+  phone: String
+  accountManagername: String
+  accountManageremail: String
+  accountManagerphone: String
+}
+
+scalar Date
+type Jobs {
+  _id: ID
+  postDate: Date
+  jobTitle: String
+  jobLocation: String
+  jobType: String
+  salary: String
+  jobDescription: String
+  skills: [String]
+  employer: Employer
+}
+
   type User {
     _id: ID!
-    firstName: String!
-    lastName: String!
-    userName: String!
-    email: String!
-    phone: String!
-    password: String!
+    firstName: String
+    lastName: String
+    userName: String
+    email: String
+    phone: String
+    password: String
+    resume: String
+    skill: [String]
+    locationPreference: String
+    jobtypePreference: String
+    salaryRange: String
+    savedJobs: [Jobs]
+  }
+
+
+  type JobPackage {
+    _id: ID
+    packageName: String
+    packageDuration: String
+    price: Float
+  }
+
+  type Auth {
+    token: ID
+    user: User
+    employer: Employer
+
+  type Query {
+    user(id: ID!): User
+    employer(id: ID!): Employer
+    jobs: [Jobs]
+  }
+
+  input NewUserInput {
+    firstName: String
+    lastName: String
+    userName: String
+    email: String
+    phone: String
+    password: String
     resume: String
     skill: [String]
     locationPreference: String
@@ -16,42 +79,44 @@ const typeDefs = gql`
     salaryRange: String
   }
 
-  type Employer {
-    _id: ID
-    companyName: String!
-    address: String!
-    city: String!
-    state: String!
-    website: String!
-    userName: String!
-    email: String!
-    password: String!
-    phone: String!
-    accountManagername: String!
-    accountManageremail: String!
-    accountManagerphone: String!
+  input NewEmployerInput {
+    companyName: String
+    address: String
+    city: String
+    state: String
+    website: String
+    userName: String
+    email: String
+    password: String
+    phone: String
+    accountManagername: String
+    accountManageremail: String
+    accountManagerphone: String
   }
 
-  scalar Date
-  type Jobs {
-    _id: ID!
+  input NewJobInput {
     postDate: Date
-    jobTitle: String!
-    jobLocation: String!
-    jobType: String!
-    salary: String!
-    jobDescription: String!
-    skills: [String!]
-    employer: Employer
-    users: [User]
+    jobTitle: String
+    jobLocation: String
+    jobType: String
+    salary: String
+    jobDescription: String
+    skills: [String]
   }
 
-  type JobPackage {
-    _id: ID!
-    packageName: String
-    packageDuration: String
-    price: Float
+
+  type Mutation {
+    login(email: String, password: String): Auth
+    addUser(user: NewUserInput): Auth
+    addEmployer(employer: NewEmployerInput): Auth
+    
+    addJob(job: NewJobInput): Job
+    saveJob(id: ID!): User
+    removeJob(id: ID!): User
   }
+
+
+
 `;
 
 module.exports = typeDefs;
