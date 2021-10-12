8f4b905e31b5ab45dbf0070adc31b1f3a96d2f33
kalyani joshi <kalyani121@gmail.com>
2021-10-12 11:42:00 -0400

    updated folder structure for client and moved example code to seaparate folders

--- client/src/pages/example/Detail.js
+++ client/src/pages/example/Detail.js
@@ -3,15 +3,15 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+import { useStoreContext } from '../../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
   ADD_TO_CART,
   UPDATE_PRODUCTS,
-} from '../utils/actions';
-import { QUERY_PRODUCTS } from '../utils/queries';
-import { idbPromise } from '../utils/helpers';
+} from '../../utils/actions';
+import { QUERY_PRODUCTS } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
