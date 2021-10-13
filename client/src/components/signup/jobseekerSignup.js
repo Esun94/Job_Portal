70c1ed1e316b46a8a3e7a46644ddb27684f8a268
kalyani joshi <kalyani121@gmail.com>
2021-10-13 12:02:46 -0400

    added jobseeker signup form fields

--- client/src/components/signup/jobseekerSignup.js
+++ client/src/components/signup/jobseekerSignup.js
@@ -37,29 +37,7 @@ const JobseekerSignup = () => {
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
         </Row>
-
-        <Form.Group className="mb-3" controlId="formGridAddress1">
-          <Form.Label>Address</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
-        </Form.Group>
-
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control type="text" placeholder="Enter City" />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Control type="text" placeholder="Enter State" />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control type="text" placeholder="Enter Zipcode" />
-          </Form.Group>
-        </Row>
-
+        
         <Form.Group className="mb-3" controlId="formGridPhone">
           <Form.Label>Phone</Form.Label>
           <Form.Control placeholder="Enter phone number with area code" />
