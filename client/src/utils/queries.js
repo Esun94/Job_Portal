ab5a3fa055bddfdacbb2a315326325d5a0ddd4c5
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-10-13 14:34:00 -0400

    saving queries and mutations files

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -20,8 +20,26 @@ export const GET_USER = gql`
           jobTitle
         }
     }
-`
+`;
 
 export const GET_EMPLOYER = gql`
-    
+query employer ($id: ID!) {
+    employer(id:$id) {
+          companyName
+          address
+          city
+          state
+          website
+          userName
+          email
+          phone
+          accountManagername
+          accountManageremail
+          accountManagerphone
+    }
+  }
+`;
+
+export const GET_JOBS = gql`
+  
 `
