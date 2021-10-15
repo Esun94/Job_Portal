42ea95b8c0eacf9a8a1fc2d736b01aec85d5f97c
kalyani joshi <kalyani121@gmail.com>
2021-10-15 15:52:22 -0400

    fixed search job issue

--- client/src/components/jobCard.js
+++ client/src/components/jobCard.js
@@ -1,69 +1,51 @@
-import React, {useEffect} from 'react';
-import { Form, Col, Row, Button, Card, ListGroup, ListGroupItem, Container } from 'react-bootstrap';
-import { useQuery, gql } from '@apollo/client';
-import { GET_JOBS } from '../utils/queries';
-import { APPLY_JOB } from '../utils/mutations';
+import React from 'react';
+import { Col, Row, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { GET_JOBS } from '../utils/queries';
 
+const JobCard = ({ jobTitle }) => {
 
-const JobCard = () => {
+  const { error, loading, data } = useQuery(GET_JOBS, {
+    variables: { jobTitle },
+  });
+  const jobs = data?.jobs || [];
 
-    const applyJob
+  const handleApplyJob = (event) => {
+    // TODO:
+    // 1. get the Job ID
+    // 2. call APPLY_JOB based on user logged in
+    // 3. If not logged then redirect to Login USer/JobSeeker
     
-    const { error, loading, data } = useQuery(GET_JOBS)
-    const jobs = data?.jobs || [];
+  }
 
-    // useEffect(() => {
-
-    // }, [data]);
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-    
-        try {
-            const { data } = await addJob({
-                variables: { ...userFormData },
-            });
-    
-            if (!data) {
-                throw new Error('something went wrong!');
-            }
-    
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    return (
-        <Row xs={1} md={3} className="g-4">
-            {jobs.map((job) => (
-                <Col key={job._id}>
-                <Card style={{ width: 'auto'}} >
-                    {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
-                    <Card.Body>
-                        <Card.Title>{job.jobTitle}</Card.Title>
-                        <Card.Text>
-                        {job.jobDescription}
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup className="list-group-flush">
-                        <ListGroupItem>{job.jobLocation}</ListGroupItem>
-                        <ListGroupItem>{job.salary}</ListGroupItem>
-                        <ListGroupItem>{job.employer.companyName}</ListGroupItem>
-                    </ListGroup>
-                    <Card.Body>{Auth.loggedIn() ? 
-                    <Card.Link href="#">Apply Now</Card.Link> : 
-                    <Card.Link href="#">Login to Apply</Card.Link>}
-                    </Card.Body>
-                </Card>
-            </Col>
-            ))
-            }
-        </Row>
-    )
-}
-
-
-
+  return (
+    <Row xs={1} md={3} className="g-4">
+      {jobs.map((job) => (
+        <Col key={job._id}>
+          <Card style={{ width: 'auto' }}>
+            {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
+            <Card.Body>
+              <Card.Title>{job.jobTitle}</Card.Title>
+              <Card.Text>{job.jobDescription}</Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>{job.jobLocation}</ListGroupItem>
+              <ListGroupItem>{job.salary}</ListGroupItem>
+              <ListGroupItem>{job.employer.companyName}</ListGroupItem>
+            </ListGroup>
+            <Card.Footer>
+              <Card.Link>
+                <Button type="button" onClick={handleApplyJob}>
+                  Apply
+                </Button>
+              </Card.Link>
+            </Card.Footer>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
 export default JobCard;
